c882f7fdebecbc57ff33b7dc1b5384634684a969
Vrukodar <jsvrukodar05052000@gmail.com>
2023-07-24 16:11:56 +0530

    lifted states from board to app and declared winner

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
 import './styles.scss';
+import { useState } from 'react';
 import Board from './components/Board';
+import { calculateWinner } from './winner';
 
 function App() {
+  const [Squares, setSquares] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(false);
+
+  const winner = calculateWinner(Squares);
+  const nextPlayer = isXNext ? 'X' : 'O';
+  const statusMessage = winner
+    ? `Winner is ${winner}`
+    : `Next player is ${nextPlayer}`;
+
+  const handleSquareClick = clickedPosition => {
+    if (Squares[clickedPosition] || winner) {
+      return;
+    }
+    setSquares(currentSquares => {
+      return currentSquares.map((squareValue, position) => {
+        if (clickedPosition === position) {
+          return isXNext ? 'X' : 'O';
+        }
+        return squareValue;
+      });
+    });
+    setIsXNext(currentIsXNext => !currentIsXNext);
+  };
   return (
     <div className="app">
-      <Board />
+      <h2>{statusMessage}</h2>
+      <Board Squares={Squares} handleSquareClick={handleSquareClick} />
     </div>
   );
 }
--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,25 +1,6 @@
-import { useState } from 'react';
 import Square from './Square';
 
-const Board = () => {
-  const [Squares, setSquares] = useState(Array(9).fill(null));
-  const [isXNext, setIsXNext] = useState(false);
-
-  const handleSquareClick = clickedPosition => {
-    if (Squares[clickedPosition]) {
-      return;
-    }
-    setSquares(currentSquares => {
-      return currentSquares.map((squareValue, position) => {
-        if (clickedPosition === position) {
-          return isXNext ? 'X' : '0';
-        }
-        return squareValue;
-      });
-    });
-    setIsXNext(currentIsXNext => !currentIsXNext);
-  };
-
+const Board = ({ Squares, handleSquareClick }) => {
   const renderSquare = position => {
     return (
       <Square
